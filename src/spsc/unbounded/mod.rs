d678b8a262267e2bd25eaf1d4f037965a15be52a
Julius Janeliunas <julius.janeliunas@gmail.com>
2023-11-17 20:48:33 +0200

    Finalize spsc::unbounded

--- src/spsc/unbounded/mod.rs
+++ src/spsc/unbounded/mod.rs
@@ -1,13 +1,18 @@
+use crate::util::marker::PhantomUnsync;
+
+use std::{fmt::Debug, ops::Deref};
+
 mod inner;
+
 pub use crate::error::{RecvError, SendError, TryRecvError};
 
 pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
     let (h1, h2) = inner::Inner::<T>::allocate();
-    (Sender(h1), Receiver(h2))
+    (Sender(h1, PhantomUnsync {}), Receiver(h2, PhantomUnsync {}))
 }
 
-pub struct Sender<T>(inner::InnerHolder<T>);
-pub struct Receiver<T>(inner::InnerHolder<T>);
+pub struct Sender<T>(inner::InnerHolder<T>, PhantomUnsync);
+pub struct Receiver<T>(inner::InnerHolder<T>, PhantomUnsync);
 
 impl<T> Sender<T> {
     pub fn send(&self, item: T) -> Result<(), SendError<T>> {
@@ -34,6 +39,28 @@ impl<T> Drop for Sender<T> {
         self.0.drop_count.fetch_add(1, AcqRel);
         self.0.unpark_receiver();
         // InnerHolder does the rest
+    }
+}
+
+impl<T> Debug for Sender<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "spsc::unbounded::Sender<{}> {{ channel: {:p} }}",
+            std::any::type_name::<T>(),
+            self.0.deref() as *const _
+        )
+    }
+}
+
+impl<T> Debug for Receiver<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "spsc::unbounded::Receiver<{}> {{ channel: {:p} }}",
+            std::any::type_name::<T>(),
+            self.0.deref() as *const _
+        )
     }
 }
 
